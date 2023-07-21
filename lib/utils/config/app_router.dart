5b3863dfe1d56ec4c274dde8f3636dd9ced8af41
Sajadio <sjd04969@gmail.com>
2023-07-21 23:32:10 +0300

    setup router app

---
+++ lib/utils/config/app_router.dart
@@ -0,0 +1,17 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter_news_app_clean_architecture/presentation/screens/details_screen.dart';
+import 'package:flutter_news_app_clean_architecture/presentation/screens/search_screen.dart';
+
+import '../../presentation/screens/home_screen.dart';
+
+part 'app_router.gr.dart';
+
+@AutoRouterConfig(replaceInRouteName: "Screen,Route")
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(page: HomeRoute.page, initial: true),
+        AutoRoute(page: SearchRoute.page),
+        AutoRoute(page: DetailsRoute.page),
+      ];
+}
